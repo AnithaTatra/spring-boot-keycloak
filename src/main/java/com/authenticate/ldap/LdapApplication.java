7be041f87d81fd12a24ce20bea84977b7e5a8c94
AnithaTatra <anitha.platosys@gmail.com>
2023-08-04 21:33:58 +0530

    ldap authentication

---
+++ src/main/java/com/authenticate/ldap/LdapApplication.java
@@ -0,0 +1,16 @@
+package com.authenticate.ldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+
+
+public class LdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapApplication.class, args);
+	}
+
+}
